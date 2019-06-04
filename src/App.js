9cf4c44c0118cf8171d6b5fec10fb73428201e5c
Ashlee Hayes <hayesashlee88@gmail.com>
2019-06-04 10:44:18 -0400

    create state and conditional logic

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import React from "react"
 // Then, give your best shot at rendering the word "in" if the user is logged in
 // or "out" if the user is logged out.
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn : true
+    }
+  }
+  render() {
+    let wordDisplay
+    if (this.state.isLoggedIn) {
+      wordDisplay = "in"
+    } else {
+      wordDisplay = "out"
+    }
+
     return (
         <div>
-            <h1>You are currently logged (in/out)</h1>
+            <h1>You are currently logged {wordDisplay}</h1>
         </div>
     )
+  }
 }
 
 export default App
